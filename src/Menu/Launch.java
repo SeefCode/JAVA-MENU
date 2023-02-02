5a4b513df1ec9a6f2276784b6025542270b41353
Seef Almostafa <s19ca5@umwelt-campus.de>
2023-02-02 21:28:50 +0100

    implemented new methods to the Menu class

--- src/Menu/Launch.java
+++ src/Menu/Launch.java
@@ -1,7 +1,7 @@
 package Menu;
 
 public class Launch {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Menu menu = new Menu();
         menu.run();
     }
--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -1,7 +1,13 @@
 package Menu;
+import static java.util.Comparator.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Collection;
 import java.util.Scanner;
+import java.lang.Thread;
+
 
 public class Menu {
     private ArrayList <Integer> list;
@@ -10,7 +16,10 @@ public class Menu {
     private final int ADD = 1;
     private final int REMOVE = 2;
     private final int PRINT = 3;
-    private final int EXIT = 4;
+    private final int SORT = 4;
+
+    private final int SIZE = 5;
+    private final int EXIT = 6;
 
 
     // Default Constructor
@@ -32,6 +41,13 @@ public class Menu {
         else if(this.choice == PRINT){
             System.out.println(list);
         }
+        else if(this.choice == SORT){
+            sortList();
+            System.out.println(list);
+        }
+        else if(this.choice == SIZE){
+            getSize();
+        }
         else if(this.choice == EXIT){
             return;
         }
@@ -42,10 +58,10 @@ public class Menu {
      * The possible choices are [ADD,REMOVE,PRINT,EXIT]
      */
     public void readChoice() {
-        System.out.print("Enter your choice [1-4]:");
+        System.out.print("Enter your choice [1-6]:");
         this.choice = in.nextInt();
-        boolean condition = (this.choice != ADD) || (this.choice != REMOVE) || (this.choice != PRINT) || (this.choice != EXIT);
-        check(condition, "Wrong input, your choice should be [1-4]");
+        boolean condition = (this.choice != ADD) || (this.choice != REMOVE) || (this.choice != PRINT) || (this.choice != SORT) || (this.choice != EXIT) || (this.choice != SIZE);
+        check(condition, "Wrong input, your choice should be [1-6]");
     }
 
     /**
@@ -64,7 +80,7 @@ public class Menu {
         int size;
         System.out.print("[Enter the capacity of the list]:");
         size = in.nextInt();
-        check(size >=0,"Size should be bigger than 0");
+        check(size > 0,"Size should be bigger than 0");
         return size;
     }
 
@@ -76,9 +92,13 @@ public class Menu {
                 "1. Add Element\n"+
                 "2. remove Element\n"+
                 "3. print List\n"+
-                "4. Exit Program\n"
+                "4. sort List\n"+
+                "5. List size\n"+
+                "6. Exit Program\n"
         );
+    }
 
+    public void logic(){
         switch (getChoice()){
             case ADD:
                 printOutput("[Enter number]:","Element added");
@@ -88,6 +108,12 @@ public class Menu {
                 break;
             case PRINT:
                 printOutput("[List] -> ","List printed");
+                break;
+            case SORT:
+                printOutput("[Sorted List] -> ","Soretd List printed");
+                break;
+            case SIZE:
+                printOutput("[List size] -> ","Size printed");
                 break;
             case EXIT:
                 printOutput("All work was saved !\n","Program exited");
@@ -117,14 +143,48 @@ public class Menu {
     /**
      * Methode to launch the program
      */
-    public void run(){
+    public void run() throws InterruptedException {
         init();
         do
         {
+            clrscr();
             displayMenu();
             readChoice();
+            logic();
+            Thread.sleep(1000);
         }
         while ((choice != EXIT));
+    }
+
+
+    /**
+     * This methode sorts a list when it is not empty
+     */
+    public void sortList(){
+        check(!list.isEmpty(),"List has no elements to be sorted");
+        list.sort(naturalOrder());
+    }
+
+
+    /**
+     * This methode clear and reset the screen printed list
+     */
+    public static void clrscr(){
+        //Clears Screen in java
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+        } catch (IOException | InterruptedException ex) {}
+    }
+
+
+    /**
+     * This methode returns the size of a given list
+     */
+    public void getSize(){
+        System.out.println(list.size());
     }
 
     /**
@@ -133,6 +193,7 @@ public class Menu {
      * @param msg the thrown message as a String by possible Illegal cases
      * @return the result as boolean datatype
      */
+
     public boolean check(boolean condition,String msg){
         if(!condition)
             throw new IllegalArgumentException(msg);
